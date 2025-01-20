7d119c8059795aeb66ccce9cec0f67906f685592
srija-29 <CESC@DESKTOP-F07B7E0>
2025-01-21 01:30:02 +0530

    Initial Project

---
+++ src/test/java/sampletests/NewTest.java
@@ -0,0 +1,18 @@
+package sampletests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void search() {
+	  driver=new ChromeDriver();
+	  driver.get("https://www.google.com/");
+	  WebElement s=driver.findElement(By.id("input"));
+	  s.sendKeys("Learn selenium");
+  }
+}
